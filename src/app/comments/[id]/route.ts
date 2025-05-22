3f21e4f090dd1211f7208372f1b72a8a693a57d6
MogesBekele <mogesbekele32@gmail.com>
2025-05-22 16:13:31 +0300

    dynamic route handler

---
+++ src/app/comments/[id]/route.ts
@@ -0,0 +1,17 @@
+import { comments } from "../data";
+
+export async function GET(
+  _request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
+
+  const comment = comments.find((comments) => comments.id === id);
+
+  if (!comment) {
+    return new Response("Comment not found", {
+      status: 404,
+    });
+  }
+  return Response.json(comment);
+}
