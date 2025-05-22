cc94d7f14585e9bf6e0b2832c928ec3a8f5024da
MogesBekele <mogesbekele32@gmail.com>
2025-05-22 16:36:35 +0300

    commit on the patch method

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -6,12 +6,33 @@ export async function GET(
 ) {
   const { id } = await params;
 
-const comment = comments.find((comment)=>comment.id === id)
+  const comment = comments.find((comment) => comment.id === id);
 
   if (!comment) {
     return new Response("Comment not found", {
       status: 404,
     });
   }
+  return Response.json(comment);
+}
+
+//patch
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
+  const comment = comments.find((comment) => comment.id === id);
+
+  if (!comment) {
+    return new Response("Comment not found", {
+      status: 404,
+    });
+  }
+
+  const updatedComment = await request.json();
+  comment.text = updatedComment.text;
+
   return Response.json(comment);
 }
