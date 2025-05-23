3d4e6aba43a11409f41c45b8e5b03c8a376be9a4
MogesBekele <mogesbekele32@gmail.com>
2025-05-23 17:28:21 +0300

    middleware methods in next js

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,12 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest){
-
-
-  if(request.nextUrl.pathname==="/profile"){
-  return NextResponse.redirect(new URL('hello', request.nextUrl))
-}
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/profile") {
+    return NextResponse.redirect(new URL("hello", request.nextUrl));
+  }
   // return NextResponse.redirect(new URL('/', request.url));
 }
 export const config = {
-  matcher: "/profile"
-}
+  matcher: "/profile",
+};
