f56be811781bdf4c0d1ae42bd0a5426e413fae02
MogesBekele <mogesbekele32@gmail.com>
2025-05-23 17:27:56 +0300

    middleware methods

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,12 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest){
-  // return NextResponse.redirect(new URL('/', request.url));
+
 
   if(request.nextUrl.pathname==="/profile"){
   return NextResponse.redirect(new URL('hello', request.nextUrl))
 }
+  // return NextResponse.redirect(new URL('/', request.url));
 }
 export const config = {
   matcher: "/profile"
