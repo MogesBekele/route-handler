890b6a9e0efcf1f76cd81e1c061957f4e965124e
MogesBekele <mogesbekele32@gmail.com>
2025-05-23 13:00:39 +0300

    complete the header in route

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,13 +1,12 @@
 import { headers } from "next/headers";
 import { NextRequest } from "next/server";
 export async function GET(request: NextRequest) {
+  const headersList = await headers();
+  const authHeader = headersList.get("Authorization");
 
-  const headersList =await headers();
-  const authHeader = headersList.get('Authorization');
-
-  console.log(authHeader)
+  console.log(authHeader);
 
   // const requestHearers = new Headers(request.headers);
   // console.log(requestHearers.get('Authorization'));
-  return new Response('profile data!');
+  return new Response("profile data!");
 }
