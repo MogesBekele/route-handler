a86007f9e5662a4bf9d9ca5d06b275ac5a5f5e93
MogesBekele <mogesbekele32@gmail.com>
2025-05-23 13:52:02 +0300

    get the cookies from the server

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 export async function GET(request: NextRequest) {
   const headersList = await headers();
@@ -6,7 +6,19 @@ export async function GET(request: NextRequest) {
 
   console.log(authHeader);
 
+  // const theme = request.cookies.get("theme");
+  // console.log(theme);
+  const cookieStore = await cookies();
+  const theme2 = cookieStore.get("theme");
+
+  console.log(theme2);
+
   // const requestHearers = new Headers(request.headers);
   // console.log(requestHearers.get('Authorization'));
-  return new Response("profile data!");
+  return new Response("profile data!",{
+    headers:{
+      "COntent-Type": "application/json",
+      "set-Cookie": "theme=dark",
+    }
+  });
 }
