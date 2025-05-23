f27ba12426fcad1c367f8d28ab15457baa073f57
MogesBekele <mogesbekele32@gmail.com>
2025-05-23 12:58:39 +0300

    get the header from the request data and console log it

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,8 +1,13 @@
-import { NextApiRequest } from "next";
+import { headers } from "next/headers";
 import { NextRequest } from "next/server";
 export async function GET(request: NextRequest) {
 
-  const requestHearers = new Headers(request.headers);
-  console.log(requestHearers.get('Authorization'));
+  const headersList =await headers();
+  const authHeader = headersList.get('Authorization');
+
+  console.log(authHeader)
+
+  // const requestHearers = new Headers(request.headers);
+  // console.log(requestHearers.get('Authorization'));
   return new Response('profile data!');
 }
