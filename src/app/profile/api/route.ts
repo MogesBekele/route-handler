8b81b2bed78d1727d13b931c1afcb3566b5eb5c3
MogesBekele <mogesbekele32@gmail.com>
2025-05-23 12:53:59 +0300

    add the header on the request

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,8 @@
-export async function GET(request: Request) {
+import { NextApiRequest } from "next";
+import { NextRequest } from "next/server";
+export async function GET(request: NextRequest) {
+
+  const requestHearers = new Headers(request.headers);
+  console.log(requestHearers.get('Authorization'));
   return new Response('profile data!');
 }
