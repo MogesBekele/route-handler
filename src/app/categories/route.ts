3f62e615f8da8071f4d48968213440148f214876
MogesBekele <mogesbekele32@gmail.com>
2025-05-24 17:50:09 +0300

    make force static

--- src/app/categories/route.ts
+++ src/app/categories/route.ts
@@ -1,18 +1,17 @@
-export const dynamic = 'force-static'; // This will make the route static
+export const dynamic = "force-static"; // This will make the route static
 
-export async function GET(){
-const categories=[
+export async function GET() {
+  const categories = [
+    { id: 1, name: "Technology" },
+    { id: 2, name: "Health" },
+    { id: 3, name: "Finance" },
+    { id: 4, name: "Travel" },
+    { id: 5, name: "Food" },
+    { id: 6, name: "Lifestyle" },
+    { id: 7, name: "Education" },
+    { id: 8, name: "Entertainment" },
+    { id: 9, name: "Sports" },
+  ];
 
-  { id: 1, name: "Technology" },
-  { id: 2, name: "Health" },
-  { id: 3, name: "Finance" },
-  { id: 4, name: "Travel" },
-  { id: 5, name: "Food" },
-  { id: 6, name: "Lifestyle" },
-  { id: 7, name: "Education" },
-  { id: 8, name: "Entertainment" },
-  { id: 9, name: "Sports" },
-]
-
-return Response.json(categories)
+  return Response.json(categories);
 }
