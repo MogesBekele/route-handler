39f8043f6b8b4ba92451c0cf9b267c6f9f53db62
MogesBekele <mogesbekele32@gmail.com>
2025-05-24 17:48:38 +0300

    commit on the profile page layout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,7 @@
 export default function Profile(){
   return(
     <div>
-      <h1 className="text-4xl font-bold">Profile Page</h1>
-      <p className="mt-4 text-lg">
-        This is the profile page of the user.
-      </p>
-      <p className="mt-4 text-lg">
-        You can add more details about the user here.
-      </p>
+ 
     </div>
   )
 }
