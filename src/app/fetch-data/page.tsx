837dfb5612616839bbe7f0d148a69f8060a1bd07
MogesBekele <mogesbekele32@gmail.com>
2025-05-25 17:59:23 +0300

    fetch data

--- src/app/fetch-data/page.tsx
+++ src/app/fetch-data/page.tsx
@@ -1,21 +1,18 @@
 "use client";
-import React, { useEffect, useState } from "react";
-export default function FetchDataPage() {
-  const [data, setData] = useState([])
-  
-  // Simulate fetching data from an API or database
-useEffect(()=>{
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((data) => {
-      setData(data.slice(0, 10)); // Limit to first 10 items for simplicity
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-      setData([])
-    });
+import React, { useState, useEffect } from "react";
 
-},[])
+export default function FetchDataPage() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setData(data.slice(0, 10)))
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setData([]);
+      });
+  }, []);
 
   return (
     <div>
