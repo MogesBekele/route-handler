4a498b3eb8a9dda535d8f780ebcf749b2c788a78
MogesBekele <mogesbekele32@gmail.com>
2025-05-25 18:21:37 +0300

    change id to string value

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -4,11 +4,14 @@ export async function GET(
   _request: Request,
   { params }: { params: { id: string } }
 ) {
-  const { id } = params;
-  const comment = comments.find((comment) => comment.id === Number(id));
+  const { id } = await params;
+
+  const comment = comments.find((comment) => comment.id === id);
 
   if (!comment) {
-    return new Response("Comment not found", { status: 404 });
+    return new Response("Comment not found", {
+      status: 404,
+    });
   }
   return Response.json(comment);
 }
@@ -17,11 +20,13 @@ export async function PUT(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  const { id } = params;
-  const comment = comments.find((comment) => comment.id === Number(id));
+  const { id } = await params;
+  const comment = comments.find((comment) => comment.id === id);
 
   if (!comment) {
-    return new Response("Comment not found", { status: 404 });
+    return new Response("Comment not found", {
+      status: 404,
+    });
   }
 
   const updatedComment = await request.json();
@@ -29,13 +34,12 @@ export async function PUT(
 
   return Response.json(comment);
 }
-
 export async function DELETE(
   _request: Request,
   { params }: { params: { id: string } }
 ) {
   const { id } = params;
-  const commentIndex = comments.findIndex((comment) => comment.id === Number(id));
+  const commentIndex = comments.findIndex((comment) => comment.id === id);
   if (commentIndex === -1) {
     return new Response("Comment not found", { status: 404 });
   }
