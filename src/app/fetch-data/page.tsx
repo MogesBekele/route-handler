b13bd272347f31c459ec83f5cc39517273ac3c97
MogesBekele <mogesbekele32@gmail.com>
2025-05-25 18:04:17 +0300

    fetch data from both server and client component

--- src/app/fetch-data/page.tsx
+++ src/app/fetch-data/page.tsx
@@ -1,18 +1,7 @@
-"use client";
-import React, { useState, useEffect } from "react";
-
-export default function FetchDataPage() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((data) => setData(data.slice(0, 10)))
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setData([]);
-      });
-  }, []);
+export default async function FetchDataPage() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const allData = await res.json();
+  const data = allData.slice(0, 10);
 
   return (
     <div>
