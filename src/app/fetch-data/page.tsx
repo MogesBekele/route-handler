1ded06797ec7eca5e5396af0377eb18171ff5d8f
MogesBekele <mogesbekele32@gmail.com>
2025-05-25 17:48:01 +0300

    fetch data from the api

--- src/app/fetch-data/page.tsx
+++ src/app/fetch-data/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+import React, { useState } from "react";
 export default async function FfetchDataPage() {
+  const [data, setData] = useState([])
+  
   // Simulate fetching data from an API or database
-  const data = await fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((data) => data.json())
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((data) => {
+      setData(data.slice(0, 10)); // Limit to first 10 items for demonstration
+      return data;
+    })
     .catch((error) => {
       console.error("Error fetching data:", error);
       return [];
