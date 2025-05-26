d49c2940d128242d9d17af11d5ec729ac1058fde
MogesBekele <mogesbekele32@gmail.com>
2025-05-26 16:19:04 +0300

    apply tailwind css

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 export default function DashboardPage() {
   const [name, setName]= useState('')
   return (
-    <div>
-      <h1>dashboard</h1>
+    <div className="flex flex-col items-center justify-center min-h-34 bg-amber-800 text-white">
+      <h1 className="">dashboard</h1>
       <p>{name}</p>
       <input type="text"  value={name} onChange={(e)=>setName(e.target.value)} />
    
