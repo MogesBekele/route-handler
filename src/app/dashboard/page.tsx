ae1ebb6deb409dd06c8e8e48c1a8545222ee3a92
MogesBekele <mogesbekele32@gmail.com>
2025-05-26 16:21:30 +0300

    applied tailwind css on dashboard component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,9 +5,9 @@ export default function DashboardPage() {
   const [name, setName]= useState('')
   return (
     <div className="flex flex-col items-center justify-center min-h-34 bg-amber-800 text-white">
-      <h1 className="">dashboard</h1>
+      <h1 className="text-4xl">dashboard</h1>
       <p>{name}</p>
-      <input type="text"  value={name} onChange={(e)=>setName(e.target.value)} />
+      <input className="bg-white text-black"  type="text"  value={name} onChange={(e)=>setName(e.target.value)} />
    
     </div>
   );
