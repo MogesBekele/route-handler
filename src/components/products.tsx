745ee92b6aaed084e458d816001a031070fb45e1
MogesBekele <mogesbekele32@gmail.com>
2025-05-27 15:55:38 +0300

    streaming ability

---
+++ src/components/products.tsx
@@ -0,0 +1,8 @@
+export const Product=async ()=>{
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return(
+    <div>
+      <h1>this is product  component</h1>
+    </div>
+  )
+}
---
+++ src/components/reviews.tsx
@@ -0,0 +1,8 @@
+export const Reviews =async ()=>{
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return(
+    <div>
+      <h1>this is review component</h1>
+    </div>
+  )
+}
