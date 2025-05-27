1e876528eef2f03ac4113e6d71f386ef1b455b00
MogesBekele <mogesbekele32@gmail.com>
2025-05-27 16:00:19 +0300

    suspense and fallback proprties

--- src/app/product-reviews/page.tsx
+++ src/app/product-reviews/page.tsx
@@ -1,13 +1,17 @@
 import { Product } from "@/components/products";
 import { Reviews } from "@/components/reviews";
+import { Suspense } from "react";
 
 export default function ProductReviewsPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-34 bg-amber-800 text-white">
       <h1 className="text-4xl">Product Reviews</h1>
-     <Product/>
-     <Reviews/>
-    
+      <Suspense fallback={<div>Loading Product...</div>}>
+        <Product />
+      </Suspense>
+      <Suspense fallback={<div>Loading Reviews...</div>}>
+        <Reviews />
+      </Suspense>
     </div>
   );
 }
