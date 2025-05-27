d13fdc9d7183a0f2b08c9504f6fd4298498222e5
MogesBekele <mogesbekele32@gmail.com>
2025-05-27 16:34:28 +0300

    how to use the context on the page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,16 @@
 "use client"
 import { useState } from "react";
+import { useTheme } from "@/components/theme-provider";
 
 export default function DashboardPage() {
   const [name, setName]= useState('')
+  const theme =useTheme()
   return (
     <div className="flex flex-col items-center justify-center min-h-34 bg-amber-800 text-white">
       <h1 className="text-4xl">dashboard</h1>
       <p>{name}</p>
       <input className="bg-white text-black"  type="text"  value={name} onChange={(e)=>setName(e.target.value)} />
+      <h1 style={{color: theme.colors.primary}}>this to check the theme color</h1>
    
     </div>
   );
