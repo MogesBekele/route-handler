77eebd85b4f778f52a2c6f364ebfe7ca021c8530
MogesBekele <mogesbekele32@gmail.com>
2025-05-28 12:45:42 +0300

    neste server component one with server compeont two

--- src/components/server-component-one.tsx
+++ src/components/server-component-one.tsx
@@ -1,21 +1,26 @@
-import fs from 'fs';
-import path from 'path';
-import { ServerComponentTwo } from './server-component-two';
+import fs from "fs";
+import path from "path";
+import { ServerComponentTwo } from "./server-component-two";
 
 export const ServerComponentOne = () => {
-  const filePath = path.join(process.cwd(), 'route-handler', 'src', 'components', 'server-component-one.tsx');
-  let fileContent = '';
+  const filePath = path.join(
+    process.cwd(),
+    "route-handler",
+    "src",
+    "components",
+    "server-component-one.tsx"
+  );
+  let fileContent = "";
   try {
-    fileContent = fs.readFileSync(filePath, 'utf-8');
+    fileContent = fs.readFileSync(filePath, "utf-8");
   } catch (e) {
-    fileContent = 'File not found';
+    fileContent = "File not found";
   }
 
   return (
     <>
       <h1>server component one</h1>
-        <ServerComponentTwo />
-   
+      <ServerComponentTwo />
     </>
   );
 };
