0e0f4605f50f68f1228df84e3741568df816587d
MogesBekele <mogesbekele32@gmail.com>
2025-05-28 12:36:33 +0300

    fix the bug in interleaving component

--- src/components/server-component-one.tsx
+++ src/components/server-component-one.tsx
@@ -1,23 +1,12 @@
 import fs from 'fs';
-import path from 'path';
 import { ServerComponentTwo } from './server-component-two';
-
 export const ServerComponentOne = () => {
-  let fileContent = '';
-  try {
-    fileContent = fs.readFileSync(
-      path.join(process.cwd(), 'src/components/server-component-one.tsx'),
-      'utf-8'
-    );
-  } catch (e) {
-    fileContent = 'File not found';
-  }
+  fs.readFileSync('/src/components/server-component-one.tsx', 'utf-8', )
+  return(
 
-  return (
     <>
       <h1>server component one</h1>
-      <pre>{fileContent.slice(0, 200)}...</pre>
       <ServerComponentTwo />
     </>
-  );
-};
+  )
+}
