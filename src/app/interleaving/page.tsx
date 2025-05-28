fff4bbb6ffdeb8042562ae9db9c816f94cd006ac
MogesBekele <mogesbekele32@gmail.com>
2025-05-28 12:55:49 +0300

    complete the interleaveing the components

--- src/app/interleaving/page.tsx
+++ src/app/interleaving/page.tsx
@@ -1,11 +1,12 @@
 
-import { ServerComponentOne } from "@/components/server-component-one";
+import { ClientComponentOne } from "@/components/client-component-one";
+
 
 export default function InterleavingPage() {
   return (
     <>
       <h1>Interleaving Page</h1>
-     <ServerComponentOne/>
+   <ClientComponentOne/>
     </>
   );
 }
