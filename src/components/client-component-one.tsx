76b51f1e8067202be491a6189b42995a7da182e8
MogesBekele <mogesbekele32@gmail.com>
2025-05-28 12:14:22 +0300

    create client component one

---
+++ src/components/client-component-one.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useState } from "react";
+export const ClientComponentOne = () => {
+  const [name, setName] = useState("John Doe");
+  return (
+    <div>
+      <h1>Client Component One</h1>
+     
+    </div>
+  );
+}
