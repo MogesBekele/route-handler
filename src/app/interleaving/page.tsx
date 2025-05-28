8e3c0f4f7e78586ffe866eb7a4a219dbf630e37e
MogesBekele <mogesbekele32@gmail.com>
2025-05-28 12:42:23 +0300

    fix the bug

--- src/app/interleaving/page.tsx
+++ src/app/interleaving/page.tsx
@@ -4,7 +4,6 @@ export default function InterleavingPage() {
   return (
     <>
       <h1>Interleaving Page</h1>
-
       <ServerComponentOne />
     </>
   );
--- src/components/server-component-one.tsx
+++ src/components/server-component-one.tsx
@@ -1,12 +1,19 @@
 import fs from 'fs';
-import { ServerComponentTwo } from './server-component-two';
-export const ServerComponentOne = () => {
-  fs.readFileSync('/src/components/server-component-one.tsx', 'utf-8', )
-  return(
+import path from 'path';
 
+export const ServerComponentOne = () => {
+  const filePath = path.join(process.cwd(), 'route-handler', 'src', 'components', 'server-component-one.tsx');
+  let fileContent = '';
+  try {
+    fileContent = fs.readFileSync(filePath, 'utf-8');
+  } catch (e) {
+    fileContent = 'File not found';
+  }
+
+  return (
     <>
       <h1>server component one</h1>
-      <ServerComponentTwo />
+   
     </>
-  )
-}
+  );
+};
