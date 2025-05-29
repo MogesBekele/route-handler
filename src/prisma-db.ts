d058e1cf1835a8aac99aba96f6048e59b6665d27
MogesBekele <mogesbekele32@gmail.com>
2025-05-29 10:47:24 +0300

    get the product from pirsma

---
+++ src/prisma-db.ts
@@ -0,0 +1,42 @@
+import {PrismaClient} from '@prisma/client';
+import { resolve } from 'path';
+
+const prisma  = new PrismaClient()
+
+const seedProducts = async ()=>{
+  const count = await prisma.product.count();
+
+  if(count ===0){
+
+    await prisma.product.createMany({
+      data: [
+        { title: 'Product 2', price: 200, description: 'Description for product 2' },
+        { title: 'Product 1', price: 100, description: 'Description for product 1' },
+        { title: 'Product 3', price: 300, description: 'Description for product 3' },
+      ],
+    });
+    console.log('Products seeded successfully');
+  }
+  
+
+}
+
+seedProducts()
+
+// create a function to get all products
+export const getProducts = async () => {
+  await new Promise((resolve)=>{setTimeout(resolve, 2000)});
+  const products = await prisma.product.findMany();
+  return products;
+}
+
+// create a function to get a product by id
+export const getProductById = async (id: number) => {
+  await new Promise((resolve)=>{setTimeout(resolve, 2000)});
+  const product = await prisma.product.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  return product;
+}
