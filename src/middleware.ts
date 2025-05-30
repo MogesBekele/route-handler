b7fd6a5c00e95ef9daa85190c4215a6cf7cf7a2f
MogesBekele <mogesbekele32@gmail.com>
2025-05-30 13:33:08 +0300

    use clerk third part libariry for autentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("hello", request.nextUrl));
-  }
-  // return NextResponse.redirect(new URL('/', request.url));
-}
+export default clerkMiddleware();
+
 export const config = {
-  matcher: "/profile",
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
