7473e75a286e642fc0c965f0eec60590f0f6887a
MogesBekele <mogesbekele32@gmail.com>
2025-05-30 13:37:40 +0300

    create the sighin button from clerk

---
+++ src/components/navigation.tsx
@@ -0,0 +1,18 @@
+export const Navigation = () => {
+  return(
+    <nav>
+      <div>
+        <div>
+          <div>
+            <h1>Next js app</h1>
+          </div>
+          <div>
+            {/*sigh in button */}
+          </div>
+        </div>
+      </div>
+   
+    </nav>
+    
+  )
+}
