e0f34e36254556a45c20c17b56f70e86a470ec5c
MogesBekele <mogesbekele32@gmail.com>
2025-05-30 13:39:15 +0300

    import the sigh in button component from clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Navigation } from "@/components/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Navigation/>
           {children}
         </body>
       </ThemeProvider>
