dab2d6175fa7f25a78356395dae9eabdda029ca3
MogesBekele <mogesbekele32@gmail.com>
2025-05-31 09:46:10 +0300

    user and session data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,25 +1,14 @@
-"use client";
-import { useState } from "react";
-import { useTheme } from "@/components/theme-provider";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
-export default function DashboardPage() {
-  const [name, setName] = useState("");
-  const { theme } = useTheme();
+export default async function DashboardPage(){
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-34 bg-amber-800 text-white">
-      <h1 className="text-4xl">dashboard</h1>
-      <p>{name}</p>
-      <input
-        className="bg-white text-black px-2 py-1 rounded mt-2"
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Enter your name"
-      />
-      <h1 style={{ color: theme.colors.primary }} className="mt-4">
-        this to check the theme color
-      </h1>
+
+  const authObject = await auth()
+  const userObject = await currentUser()
+
+  return(
+    <div>
+      <h1>dashboard</h1>
     </div>
-  );
+  )
 }
